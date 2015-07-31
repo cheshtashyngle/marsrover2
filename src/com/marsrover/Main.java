132c0446793a8ad1cab7370f3e1624c52ada9664
Cheshta <cheshu_25@yahoo.co.in>
2015-07-31 10:48:03 +0530

    [Cheshta] [#0005] Input for Instructions of rover is taken

--- src/com/marsrover/Main.java
+++ src/com/marsrover/Main.java
@@ -15,5 +15,10 @@ public class Main {
         int yCoordinate = Integer.parseInt(inputs[1]);
         char direction = inputs[2].charAt(0);
         new Rover(xCoordinate, yCoordinate, direction);
+        input = view.input();
+        inputs = input.split(" ");
+        for(int i = 0;i < inputs.length;i++) {
+            String command = inputs[i];
+        }
     }
 }
